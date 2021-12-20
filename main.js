65ab2af909889ece9f24c465fbd16a06a05598a2
Fadziso Gabrielle <fadziso@novodigital.biz>
2021-12-20 18:32:43 +0200

    Modified

--- main.js
+++ main.js
@@ -1,31 +1,4 @@
-/* get the date we are counting down to
-let countDownDate = new Date("Jan 1, 2022 00:00:00").getTime();
-
-// countdown function
-
-let countDown = setInterval(countDownFuunction = () => {
-    let now = new Date().getTime();
-
-    // time between now and 2022
-    let timeBetween = countDownDate - now;
-
-    // time calculations
-    let days = Math.floor(timeBetween / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((timeBetween % (1000 * 60 * 60 
-        * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((timeBetween % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((timeBetween % (1000 * 60)) / 1000);
-
-    // display
-    document.getElementById('timer').innerHTML = days + 'days ' + hours + 'h ' + minutes + 'mins ' + seconds + 'secs '
-
-    // when the countdown finishes - TURN UP
-    if (timeBetween < 0) {
-        clearInterval(countDown);
-        document.getElementById('timer').innerHTML = 'TURN UP!!!'
-    }
-}, 1000);*/
-
+// create variables to store the days, hours, minutes and seconds until coumtdown date
 const days = document.getElementById('days');
 const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
