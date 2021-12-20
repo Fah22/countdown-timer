49024535808f0e20db27c3764a5827665bb64ad1
Fadziso Gabrielle <fadziso@novodigital.biz>
2021-12-20 17:34:50 +0200

    Modified

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// get the date we are counting down to
+/* get the date we are counting down to
 let countDownDate = new Date("Jan 1, 2022 00:00:00").getTime();
 
 // countdown function
@@ -24,5 +24,40 @@ let countDown = setInterval(countDownFuunction = () => {
         clearInterval(countDown);
         document.getElementById('timer').innerHTML = 'TURN UP!!!'
     }
-}, 1000);
+}, 1000);*/
 
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+// get current year so that each year it keeps updating
+const currentYear = new Date().getFullYear();
+
+// get the date we are counting down to 
+const countdownDate = new Date(`January 1 ${currentYear + 1} 00:00:00`);
+
+// countdown calculations 
+const countdownFunction = () => {
+    // get current date
+    const currentDate = new Date();
+
+    // calculate the time difference between new year time and todays date
+    const timeDifference = countdownDate - currentDate;
+
+    let numDays = Math.floor(timeDifference / 1000 / 60 / 60 / 24);
+    let numHours = Math.floor(timeDifference / 60 / 60) % 24;
+    let numMins = Math.floor(timeDifference / 1000 / 60) % 60;
+    let numSecs = Math.floor(timeDifference / 1000) % 60
+    
+    // display
+    days.innerHTML = numDays;
+    hours.innerHTML = numHours;
+    minutes.innerHTML = numMins;
+    seconds.innerHTML = numSecs;
+
+
+}
+
+// set interval for changes every second 
+setInterval(countdownFunction, 1000);
